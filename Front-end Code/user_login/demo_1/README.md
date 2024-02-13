8618b1943ed0fbd8c5d3db0e53f02ad724ef6188
Leo Li <l242li@waterloo.ca>
2024-02-13 00:40:39 -0500

    second version of login code(html, css and javascript)

---
+++ Front-end Code/user_login/demo_1/README.md
@@ -0,0 +1,1 @@
+#html, css and javascript version of login code 
--- Front-end Code/user_login/demo_1/script.js
+++ Front-end Code/user_login/demo_1/script.js
@@ -1,9 +1,17 @@
-let register_btn = document.querySelector(".Register-btn");
-let login_btn = document.querySelector(".Login-btn");
-let form = document.querySelector(".Form-box");
+let register_btn=document.querySelector(".register-btn");
+let login_btn=document.querySelector(".login-btn");
+let login_nav_btn=document.querySelector(".login-nav-btn");
+let close_icon=document.querySelector(".close-icon");
+let form_box=document.querySelector(".form-box");
 register_btn.addEventListener("click", ()=>{
-    form.classList.add("change-form");
+    form_box.classList.add("change-form");
 });
 login_btn.addEventListener("click", ()=>{
-    form.classList.remove("change-form");
+    form_box.classList.remove("change-form");
+});
+close_icon.addEventListener("click", ()=>{
+    form_box.style.transform="scale(0)";
+});
+login_nav_btn.addEventListener("click", ()=>{
+    form_box.style.transform="scale(1)";
 });
